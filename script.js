ac54d2712b67cc61899d3e7b51ed782952a3545d
Nazgul Umirova <nazgul@Nurbeks-MacBook-Pro.local>
2021-09-19 10:13:10 +1000

    adding comments

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
-
 let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 let closedDoorPath ="https://content.codecademy.com/projects/chore-door/images/closed_door.svg";
-
 let startButton = document.getElementById('start');
 let currentlyPlaying = true;
+
+
 //functions
 
 const isBot = (door) =>{
